15c31ca539193d9b27f92c4ae9ca396f15b81e00
hikmani <syarifulfajar017@gmail.com>
2026-02-28 08:16:58 +0700

    Initial Main class

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String[] args) throws Exception {
+    try {
+      System.out.println("Hello World!");
+    } catch (Exception e) {
+      System.out.println("Error: " + e.getMessage());
+    }
+  }
+
+}
